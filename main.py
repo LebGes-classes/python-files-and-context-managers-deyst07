b380139f1d84dadb5f4d95c9cad2bcdc77c5d3de
deyst <nikita6a20328@gmail.com>
2026-02-28 11:22:30 +0300

    HomeWwwOrKkk

--- main.py
+++ main.py
@@ -16,26 +16,47 @@ def main() -> None:
 
     manager = ProductManager()
 
-    print("1 - Загрузить TXT")
-    print("2 - Загрузить JSON")
-    print("3 - Пустая база")
+    is_running = True
 
-    choice = input("Выбор: ")
+    while is_running:
+        print("\nВыберите источник данных: ")
+        print("1 - Загрузить TXT")
+        print("2 - Загрузить JSON")
+        print("3 - Пустая база")
 
-    if choice == "1":
-        products = storage.load_from_txt("data.txt")
-        
-        for product in products:
-            manager.add_product(product)
+        choice = input("Выбор: ")
 
-    elif choice == "2":
-        products = storage.load_from_json("data.json")
-        
-        for product in products:
-            manager.add_product(product)
+        if choice == "1":
+            products = storage.load_from_txt("data.txt")
+            
+            for product in products:
+                manager.add_product(product)
+
+            is_running = False
+
+        elif choice == "2":
+            products = storage.load_from_json("data.json")
+            
+            for product in products:
+                manager.add_product(product)
+            
+            is_running = False
+
+        elif choice == "3":
+            
+            print("Создана пустая база.")
+            is_running = False
+
+        else:
+            print("Неверный ввод. Попробуйте снова.")
 
     menu = Menu(manager, storage)
     menu.run()
+
+    save_choice = input("Сохранить изменения в JSON? (y/n): ")
+
+    if save_choice.lower() == "y":
+        storage.save_to_json("data.json", manager.products)
 
 
 if __name__ == "__main__":
